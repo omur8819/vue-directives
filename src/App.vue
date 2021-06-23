27087439481ac73c1506f188d8fa437579832945
omurbozdag <omur8819@gmail.com>
2021-06-23 18:05:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <p v-text="'writen by v-text'"></p>
+        <p v-html="'<strong>writen by html directive</strong>'"></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-color="'green'">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe tempora incidunt recusandae ab quam ratione aliquam distinctio consequatur non doloremque consequuntur ex, natus vitae quibusdam maxime laboriosam at fugiat ipsa.</p>
+        <p v-html="'<strong>writen by html directive</strong>'"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
